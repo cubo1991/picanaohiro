985551045fd7d4650e41fd789c8d010c10849514
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2022-12-01 16:52:22 -0300

    agregado de imagen

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import s from './Home.module.css'
 import vertical from '../Assets/img/vertical.jpg'; 
+import taka from '../Assets/img/taka.jpg'; 
 
 export const Home = () => {
   return (
     <div>
 
-<h1> PICA AGENCIA INQUIETA </h1>
+<h1 className={s.title}> PICA AGENCIA INQUIETA </h1>
 
-<img className={s.vertical} src= {vertical} alt={"imagen"}/>
+<div> <img className={s.vertical} src= {vertical} alt={"imagen"}/>
+<h2 className={s.subvertical}> FOTOGRAFIA</h2>
+<p className={s.pvertical}> las mejores fotos son nuestras</p>
+</div>
 
+<div> <img className={s.imgdiseño} src= {taka} alt={"imagen"}/>
+<h2 className={s.subdiseño}> DISEÑO</h2>
+<p className={s.pdiseño}>diseños de la puta madre</p>
 
+</div>
     </div>
   )
 }
