86dbb46b6b382c63fa9641ae985d15903f6f377c
cubo1991 <adavidlopezmathez@gmail.com>
2022-12-02 08:55:02 -0300

    Links Fotografia y Diseño

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import s from './Home.module.css'
 import vertical from '../Assets/img/vertical.jpg'; 
 import taka from '../Assets/img/taka.jpg'; 
+import { Link } from 'react-router-dom';
+
+
+
 
 export const Home = () => {
   return (
@@ -9,12 +13,19 @@ export const Home = () => {
 
 <h1 className={s.title}> PICA AGENCIA INQUIETA </h1>
 
-<div> <img className={s.vertical} src= {vertical} alt={"imagen"}/>
+<div> 
+  <Link to={'/fotografia'}>
+  <img className={s.vertical} src= {vertical} alt={"imagen"}/>
+  </Link>
 <h2 className={s.subvertical}> FOTOGRAFIA</h2>
 <p className={s.pvertical}> las mejores fotos son nuestras</p>
 </div>
 
-<div> <img className={s.imgdiseño} src= {taka} alt={"imagen"}/>
+<div> 
+<Link to={'/diseño'}>
+  <img className={s.imgdiseño} src= {taka} alt={"imagen"}/>
+</Link>
+
 <h2 className={s.subdiseño}> DISEÑO</h2>
 <p className={s.pdiseño}>diseños de la puta madre</p>
 
