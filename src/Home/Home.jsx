7e1d88d500a0c3fc19b7e8275f8a41839ef0ac58
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2022-12-02 11:45:24 -0300

    agregado de imagenes

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import s from './Home.module.css'
 import vertical from '../Assets/img/vertical.jpg'; 
 import taka from '../Assets/img/taka.jpg'; 
+import chipi from '../Assets/img/chipi.jpg'; 
+import jrrpalermo from '../Assets/img/jrrpalermo.jpg'; 
+import bombonera from  '../Assets/img/bombonera.jpg';
+import pampa from  '../Assets/img/pampa.jpg';
 
 export const Home = () => {
   return (
@@ -11,14 +15,35 @@ export const Home = () => {
 
 <div> <img className={s.vertical} src= {vertical} alt={"imagen"}/>
 <h2 className={s.subvertical}> FOTOGRAFIA</h2>
-<p className={s.pvertical}> las mejores fotos son nuestras</p>
+<p className={s.pvertical}> Las mejores fotos son nuestras</p>
 </div>
 
 <div> <img className={s.imgdiseño} src= {taka} alt={"imagen"}/>
 <h2 className={s.subdiseño}> DISEÑO</h2>
-<p className={s.pdiseño}>diseños de la puta madre</p>
-
+<p className={s.pdiseño}>Diseños de la puta madre</p>
 </div>
+
+<div> <img className={s.imgmapping} src= {chipi} alt={"imagen"}/>
+<h2 className={s.submapping}> MAPPING</h2>
+<p className={s.pmapping}>Te proyectamos hasta tu futuro</p>
+</div>
+
+<div> <img className={s.imgweb} src= {jrrpalermo} alt={"imagen"}/>
+<h2 className={s.subweb}> WEB</h2>
+<p className={s.pweb}> La web que soñas aca</p>
+</div>
+
+<div> <img className={s.imgvjs} src= {bombonera} alt={"imagen"}/>
+<h2 className={s.subvjs}> VJS</h2>
+<p className={s.pvjs}> Aca iria un video?</p>
+</div>
+
+<div> <img className={s.imgiluminacion} src= {pampa} alt={"imagen"}/>
+<h2 className={s.subiluminacion}> ILUMINACION</h2>
+<p className={s.piluminacion}> Prende y apaga la tv</p>
+</div>
+
+
     </div>
   )
 }
